f991151776722f2d58e71ac75b29332857431665
Kaarel Kaljurand <kaljurand@gmail.com>
2011-11-08 16:27:35 +0100

    Included a very simple widget
    which currently acts as a launcher icon.

---
+++ app/src/ee/ioc/phon/android/unitconv/provider/ArvutajaAppWidgetProvider.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2011, Institute of Cybernetics at Tallinn University of Technology
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ee.ioc.phon.android.unitconv.provider;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+import ee.ioc.phon.android.unitconv.R;
+import ee.ioc.phon.android.unitconv.Unitconv;
+
+public class ArvutajaAppWidgetProvider extends AppWidgetProvider {
+
+	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+		final int N = appWidgetIds.length;
+
+		// Perform this loop procedure for each App Widget that belongs to this provider
+		for (int i=0; i<N; i++) {
+			int appWidgetId = appWidgetIds[i];
+
+			Intent intent = new Intent(context, Unitconv.class);
+			PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+			// Get the layout for the App Widget and attach an on-click listener to the button
+			RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget);
+			views.setOnClickPendingIntent(R.id.buttonWidgetMicrophone, pendingIntent);
+
+			// Tell the AppWidgetManager to perform an update on the current app widget
+			appWidgetManager.updateAppWidget(appWidgetId, views);
+		}
+	}
+}
